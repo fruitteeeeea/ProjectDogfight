47ff10d85a8c54ae0523c54864e5311f5aac39bc
fruitteeeeea <138597264+fruitteeeeea@users.noreply.github.com>
2026-01-05 03:14:34 +0800

    0105edithome
    *少量修改

--- Unit/Enemy/enemy.gd
+++ Unit/Enemy/enemy.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	target_forward = [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT].pick_random()
 
 
+#获取带有偏差的目标方向 
 func add_random_angle(dir: Vector2, max_deg := 15.0) -> Vector2:
 	var angle_offset = randf_range(-max_deg, max_deg)
 	return dir.normalized().rotated(deg_to_rad(angle_offset))
--- ai/tasks/climb_up_or_down.gd
+++ ai/tasks/climb_up_or_down.gd
@@ -12,6 +12,17 @@
 extends BTAction
 #爬升 或降低水平高度至与玩家一致 
 
+@export var target_var: StringName = &"target"
 
 func _tick(_delta: float) -> Status:
+	var target := blackboard.get_var(target_var) as Node2D
+	if not is_instance_valid(target):
+		return FAILURE
+	
+	var a = agent as Enemy
+	if agent.global_position.y - target.global_position.y < 0:
+		a.target_forward = Vector2.DOWN
+	else :
+		a.target_forward = Vector2.UP
+		
 	return SUCCESS
